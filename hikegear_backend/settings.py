9ab8fb06c6a98d9866cdc30bd82b8704de8ec384
kajetan <katek1094@interia.pl>
2021-01-20 09:35:10 +0100

    created MyUser and Profile

--- hikegear_backend/settings.py
+++ hikegear_backend/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
+    'app',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'django_cleanup.apps.CleanupConfig',
 ]
 
 MIDDLEWARE = [
@@ -116,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'app.MyUser'
