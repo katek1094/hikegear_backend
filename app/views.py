28f05d0c4b1017c880687d477ab08fa9d9322530
kajetan <katek1094@interia.pl>
2021-02-11 14:32:52 +0100

    Everything working good?

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.mixins import CreateModelMixin, DestroyModelMixin
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
@@ -80,8 +80,6 @@ class UserViewSet(GenericViewSet, CreateModelMixin):
 class LoginView(APIView):
     @staticmethod
     def post(request):
-        print(request)
-        print(request.data)
         try:
             email = request.data['email']
             password = request.data['password']
